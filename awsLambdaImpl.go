f35e28fb7dbb4affb35f094c11dca5a4392fcbc9
Justin Fuller <justindanielfuller@gmail.com>
2018-02-28 06:37:03 -0500

    Updates to lambda implementation to support JSON input/output.

--- awsLambdaImpl.go
+++ awsLambdaImpl.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"encoding/json"
 	"github/types"
-	"github/repos"
 	"github/githubActivity"
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -11,8 +11,22 @@ type Request struct {
 	UserName string `json:"userName"`
 }
 
-func HandleRequest(_, request Request) (repos.GithubReposResponse, error) {
-	return githubActivity.Activity(types.User(request.UserName)), nil
+func HandleRequest(_, request []byte) ([]byte, error) {
+	var parsedRequest Request;
+	parseError := json.Unmarshal(request, &parsedRequest)
+
+	if parseError != nil {
+		return nil, parseError
+	}
+
+	activity := githubActivity.Activity(types.User(parsedRequest.UserName))
+	stringifiedActivity, stringifyError := json.Marshal(activity)
+
+	if stringifyError != nil {
+		return nil, stringifyError
+	}
+
+	return stringifiedActivity, nil
 }
 
 func main() {
